4378d0155da3b2013abf59fa12e6b26ab213aaa7
andrii-trebukh <andrew.trebukh@gmail.com>
2023-12-28 04:03:24 +0200

    commit 03

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import re
 from addressbook import Record, AddressBook
 
-BOOK = AddressBook()
+book = AddressBook()
 
-COMMANDS = {}
+commands = {}
 
-EXIT_FLAG = False
+exit_flag = False
 
 
 def command_handler(command):
@@ -18,9 +18,9 @@ def command_handler(command):
             except ValueError as err:
                 return err
 
-        commands = (command, ) if isinstance(command, str) else command
-        for i in commands:
-            COMMANDS[i] = wrapper
+        commands_list = (command, ) if isinstance(command, str) else command
+        for i in commands_list:
+            commands[i] = wrapper
 
         return wrapper
     return input_error
@@ -34,22 +34,22 @@ def help_command(*args):
 
 @command_handler("list")
 def list_command(*args):
-    return "\n".join(COMMANDS)
+    return "\n".join(commands)
 
 
 @command_handler(("show all", "show"))
 def show_all_command(*args):
-    if not BOOK.data:
+    if not book.data:
         raise ValueError("It's empty. There are no any records.")
-    output = [str(record) for record in BOOK.data.values()]
+    output = [str(record) for record in book.data.values()]
     return "\n".join(output)
 
 
 @command_handler(("good bye", "close", "exit", "."))
 def exit_command(*args):
-    global EXIT_FLAG
-    EXIT_FLAG = True
-    BOOK.save_csv("addressbook.csv", overwrite=True)
+    global exit_flag
+    exit_flag = True
+    book.save_csv("addressbook.csv", overwrite=True)
     return "Good bye!"
 
 
@@ -63,27 +63,27 @@ def add_command(name, phone=None, birthday=None):
     record = Record(name, birthday)
     if phone:
         record.add_phone(phone)
-    BOOK.add_record(record)
+    book.add_record(record)
     return f"New name {name} has been added"
 
 
 @command_handler("phone add")
 def add_phone_command(name, phone):
-    record = BOOK.find(name)
+    record = book.find(name)
     record.add_phone(phone)
     return f"Phone number {phone} for name {name} has been added"
 
 
 @command_handler("phone remove")
 def remove_phone_command(name, phone):
-    record = BOOK.find(name)
+    record = book.find(name)
     record.remove_phone(phone)
     return f"Phone number {phone} for name {name} has been removed"
 
 
 @command_handler("phone edit")
 def edit_phone_command(name, old_phone, new_phone):
-    record = BOOK.find(name)
+    record = book.find(name)
     record.edit_phone(old_phone, new_phone)
     return f"Phone number {old_phone} for name {name} "\
         f"has been changed to {new_phone}"
@@ -91,26 +91,26 @@ def edit_phone_command(name, old_phone, new_phone):
 
 @command_handler("remove")
 def remove_command(name):
-    BOOK.delete(name)
+    book.delete(name)
     return f"{name} phone number has been removed"
 
 
 @command_handler("phone")
 def phone_command(name):
-    record = BOOK.find(name)
+    record = book.find(name)
     return str(record)
 
 
 @command_handler("birthday")
 def set_birthday_command(name, birthday):
-    record = BOOK.find(name)
+    record = book.find(name)
     record.set_birthday(birthday)
     return f"Birthday has been set to {birthday}"
 
 
 @command_handler("find")
 def find_command(sample):
-    output = [str(record) for record in BOOK.find_sample(sample)]
+    output = [str(record) for record in book.find_sample(sample)]
     if not output:
         return "Nothing found"
     return "Contacts found:\n" + "\n".join(output)
@@ -118,9 +118,9 @@ def find_command(sample):
 
 def main():
 
-    BOOK.load_csv("addressbook.csv")
+    book.load_csv("addressbook.csv")
 
-    while not EXIT_FLAG:
+    while not exit_flag:
         input_string = input(">>> ")
         input_string = input_string.strip()
 
@@ -130,7 +130,7 @@ def main():
         input_string += " "
 
         command = None
-        for check_command in COMMANDS:
+        for check_command in commands:
 
             if input_string.lower().startswith(f"{check_command} "):
                 command = check_command
@@ -147,7 +147,7 @@ def main():
             print("No such command")
             continue
 
-        print(COMMANDS[command](*args))
+        print(commands[command](*args))
 
 
 if __name__ == "__main__":
